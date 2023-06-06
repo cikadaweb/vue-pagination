1357fc1903af580c314e01b8d8e8ed937c714b2d
CIKADA <adel.garipovazn@mail.ru>
2023-06-06 14:08:16 +0300

    Fetch data

--- src/store/products.ts
+++ src/store/products.ts
@@ -1,9 +1,16 @@
 import { defineStore } from 'pinia';
 
+import axios from 'axios';
+
 export const useProductsStore = defineStore('products', {
   state: () => ({
-    products: [];
+    products: []
   }),
   actions: {
+    async fetchData() {
+      const respone = await axios.get('https://vue-pagination-b8d64-default-rtdb.firebaseio.com/products.json');
+      const data = await respone.data;
+      this.products = data.products;
+    }
   },
 })
